f3e3c85988337a3aa18cc44b62dfd3d48c4b8f78
Patrick Kubiak <patrick@antuple.net>
2017-07-24 14:02:33 -0400

    alert in full shadow dom

---
+++ gulpfile.babel.js
@@ -0,0 +1,36 @@
+/**
+ * BS4 Polymer gulpfile
+ */
+
+'use strict';
+
+// include gulp & tools
+import gulp from 'gulp';
+import sass from 'gulp-sass';
+import cleanCSS from 'gulp-clean-css';
+import rename from 'gulp-rename';
+import inlineSource from 'gulp-inline-source';
+
+// Compile Stylesheets
+gulp.task('sass', function() {
+  return gulp.src('src/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS({
+      level: 2,
+    }))
+    .pipe(rename({
+      suffix: '.min',
+      extname: '.css',
+    }))
+    .pipe(gulp.dest('src/'));
+});
+
+// Inline css
+gulp.task('inline', ['sass'], function() {
+  return gulp.src('src/**/*.html')
+    .pipe(inlineSource())
+    .pipe(gulp.dest('dist/'));
+});
+
+// Default task
+gulp.task('default', ['sass', 'inline']);
